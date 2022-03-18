5909f86ebb2642d65b1efd2a375bdb990267444a
Dan Duta <danduta23@gmail.com>
2022-03-18 20:27:28 +0200

    Simulating producer for all the data points, cleaned up dataset header, working maven dependency cache

--- home-architekt-producer/src/main/java/io/github/danduta/producer/SensorProducerSimulator.java
+++ home-architekt-producer/src/main/java/io/github/danduta/producer/SensorProducerSimulator.java
@@ -1,23 +1,43 @@
 package io.github.danduta.producer;
 
+import kafka.admin.AdminUtils;
+import kafka.admin.AdminUtils$;
+import kafka.zk.KafkaZkClient;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVRecord;
+import org.apache.kafka.clients.admin.Admin;
+import org.apache.kafka.clients.admin.CreateTopicsResult;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.PartitionInfo;
+import org.apache.kafka.common.serialization.DoubleSerializer;
+import org.apache.kafka.common.serialization.Serializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.apache.kafka.common.serialization.UUIDSerializer;
 import org.apache.log4j.Logger;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.Properties;
+import java.sql.Time;
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.TemporalUnit;
+import java.util.*;
+import java.util.Map.Entry;
+import java.util.concurrent.ExecutionException;
+import java.util.stream.Collectors;
 
 public class SensorProducerSimulator {
 
     private static final String DATASET_CSV_PATH = "DATASET_CSV_PATH";
-    private static final String KAFKA_TOPIC = "KAFKA_TOPIC";
 
     private static final Properties props = new Properties();
+    private static final UUID producerId = UUID.randomUUID();
     private static final Logger log = Logger.getLogger(SensorProducerSimulator.class);
 
     static {
@@ -29,32 +49,87 @@ public class SensorProducerSimulator {
     }
 
     public static void main(String[] args) {
-        KafkaProducer<Long, Double> producer = new KafkaProducer<>(props);
+        Admin kafkaAdmin = Admin.create(props);
+
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, DoubleSerializer.class);
+        KafkaProducer<UUID, Double> numericalDataProducer = new KafkaProducer<>(props);
+
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        KafkaProducer<UUID, String> stringDataProducer = new KafkaProducer<>(props);
 
         try {
-            FileReader in = new FileReader(System.getenv(DATASET_CSV_PATH));
+            String datasetPath = System.getenv(DATASET_CSV_PATH);
+            assert datasetPath != null : "Could not resolve path to dataset CSV file";
+
+            FileReader in = new FileReader(datasetPath);
             CSVParser records = CSVFormat.DEFAULT.withFirstRecordAsHeader().parse(in);
 
-            log.info("Parsing started");
-            log.info("Dataset header: " + records.getHeaderMap());
+            Map<String, Integer> headerMap = records.getHeaderMap();
+            headerMap.remove("time");
 
-            String topic = System.getenv(KAFKA_TOPIC);
+            createKafkaTopics(kafkaAdmin, headerMap.keySet());
 
             for (CSVRecord record : records) {
-                ProducerRecord<Long, Double> producerRecord = new ProducerRecord<>(topic,
-                    Long.valueOf(record.get("time")),
-                    Double.valueOf(record.get("House overall [kW]")));
-
-                producer.send(producerRecord);
-                log.info(producerRecord);
+                produceRecord(numericalDataProducer, stringDataProducer, record);
             }
         } catch (FileNotFoundException e) {
             log.error("Dataset could not be found", e);
         } catch (IOException e) {
             log.error("Exception occurred while reading the dataset", e);
+        } catch (Exception e) {
+            log.error(e.getMessage());
         } finally {
-            producer.flush();
-            producer.close();
+            numericalDataProducer.flush();
+            numericalDataProducer.close();
+
+            stringDataProducer.flush();
+            stringDataProducer.close();
         }
+    }
+
+    private static void produceRecord(KafkaProducer<UUID, Double> numericalDataProducer, KafkaProducer<UUID, String> stringDataProducer, CSVRecord record) {
+        Map<String, String> recordHeaderMap = record.toMap();
+
+        for (Entry<String, String> e : recordHeaderMap.entrySet()) {
+            String topic = e.getKey();
+            String valueString = e.getValue();
+
+            try {
+                ProducerRecord<UUID, Double> producerRecord = new ProducerRecord<>(topic,
+                        producerId,
+                        Double.valueOf(valueString));
+
+                numericalDataProducer.send(producerRecord);
+            } catch (NumberFormatException ex) {
+                ProducerRecord<UUID, String> producerRecord = new ProducerRecord<>(topic,
+                        producerId,
+                        valueString);
+
+                stringDataProducer.send(producerRecord);
+            }
+        }
+    }
+
+    private static void createKafkaTopics(Admin kafkaAdmin, Set<String> newTopics) throws InterruptedException, ExecutionException {
+        KafkaConsumer<Object, Object> dummyConsumer = new KafkaConsumer<>(props);
+        Set<String> existentTopics = dummyConsumer.listTopics(Duration.of(60, ChronoUnit.SECONDS)).keySet();
+
+        log.info("Existent topics: " + existentTopics);
+
+        newTopics.removeAll(existentTopics);
+
+        if (newTopics.isEmpty()) {
+            log.info("All needed topics are already created");
+            return;
+        }
+
+        log.info("Creating new topics: " + newTopics);
+
+        Optional<Integer> partitions = Optional.empty();
+        Optional<Short> replicationFactor = Optional.of((short) 1);
+        List<NewTopic> kafkaTopics = newTopics.stream().map(column -> new NewTopic(column, partitions, replicationFactor)).collect(Collectors.toList());
+
+        CreateTopicsResult topicsCreationResult = kafkaAdmin.createTopics(kafkaTopics);
+        topicsCreationResult.all().get();
     }
 }
